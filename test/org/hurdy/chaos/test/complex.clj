fe892e01fd24c3d26a7bc88fa92f6accd2e6ecb7
Mike Hurd <leahcim77@gmail.com>
2012-09-11 11:56:54 +0200

    Updated comments

--- test/org/hurdy/chaos/test/complex.clj
+++ test/org/hurdy/chaos/test/complex.clj
@@ -2,12 +2,14 @@
   (:use [org.hurdy.chaos.complex])
   (:use [clojure.test]))
 
+;; Test that (1 + 2i) + (3 - i) = (4 + i)
 (deftest test-add []
   (is
       (= (add {:r 1 :i 2} {:r 3 :i -1}) {:r 4 :i 1})
   )
 )
 
+;; Test that (5 + 2i) * (4 + 3i) = (14 + 23i)
 (deftest test-multiply []
   (is
       (= (multiply {:r 5 :i 2} {:r 4 :i 3}) {:r 14 :i 23})
