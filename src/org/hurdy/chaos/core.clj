c54ad0741616cebe23902e58055120a23f39c6c2
Mike Hurd <leahcim77@gmail.com>
2012-09-13 21:33:22 +0100

    Modified comment

--- src/org/hurdy/chaos/core.clj
+++ src/org/hurdy/chaos/core.clj
@@ -114,6 +114,8 @@
 
 (defn render-gl [texture-id calculator width height]
     (update-fps)
+    ;; still need to work out how to replace the texture/buffer in the render loop without killing
+    ;; my machine :-)
     (. GL11 glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
     (. GL11 glEnable GL11/GL_BLEND)
     (. GL11 glBlendFunc GL11/GL_SRC_ALPHA GL11/GL_ONE_MINUS_SRC_ALPHA)
